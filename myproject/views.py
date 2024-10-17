e7a650cec8f9a72e99dc278288ddef92b3a68b98
niyazgrid <sheffman17@gmail.com>
2024-10-17 20:34:14 +0300

    restapi

---
+++ myproject/views.py
@@ -0,0 +1,42 @@
+from django.http import JsonResponse
+from .models import Food
+from .serializers import FoodSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(['GET', 'POST'])
+def food_list(request, format=None):
+
+    if request.method == "GET":
+        foods = Food.objects.all()
+        serializer = FoodSerializer(foods, many=True)
+        return Response(serializer.data)
+    
+    if request.method == "POST":
+        serializer = FoodSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+@api_view(['GET', 'PUT', 'DELETE'])
+def food_detail(request, id, format=None):
+
+    try:
+        food = Food.objects.get(pk=id)
+    except Food.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+    if request.method == "GET":
+        serializer = FoodSerializer(food)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = FoodSerializer(food, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        food.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
